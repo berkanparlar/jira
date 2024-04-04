c9f2d0212d46a10617b9e08d3edeb34aaa3d22ce
berkanparlar <berkanparlar0@gmail.com>
2024-04-05 01:50:42 +0300

    json

--- src/App.js
+++ src/App.js
@@ -1,45 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import TaksCreate from "./components/TaksCreate";
 import TaskList from "./components/TaskList";
+import axios from "axios";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  const createTask = (title, taskDesc) => {
-    const createdTasks = [
-      ...tasks,
-      {
-        id: Math.round(Math.random() * 999999),
-        title,
-        taskDesc,
-      },
-    ];
+  const createTask = async (title, taskDesc) => {
+    const response = await axios.post("http://localhost:3002/tasks", {
+      title,
+      taskDesc,
+    });
+    console.log(response);
+    const createdTasks = [...tasks, response.data];
     setTasks(createdTasks);
   };
 
-  const deleteTaskById = (id) => {
-    const afterDeletingTasks=tasks.filter((tasks)=>{
-      return tasks.id!==id
+  const fetchTasks = async () => {
+    const response = await axios.get("http://localhost:3002/tasks");
+    setTasks(response.data);
+  };
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  const deleteTaskById = async (id) => {
+    await axios.delete(`http://localhost:3002/tasks/${id}`);
+    const afterDeletingTasks = tasks.filter((tasks) => {
+      return tasks.id !== id;
     });
     setTasks(afterDeletingTasks);
   };
 
-
-  const editTaskById= (id,updatedTitle,updatedTaskDesc)=>{
-    const updateTask=tasks.map((tasks)=>{
-      if(tasks.id === id)
-      {
-        return{id,title:updatedTitle,taskDesc:updatedTaskDesc}
+  const editTaskById = async (id, updatedTitle, updatedTaskDesc) => {
+    await axios.put(`http://localhost:3002/tasks/${id}`,{title:updatedTitle,taskDesc:updatedTaskDesc});
+    const updateTask = tasks.map((tasks) => {
+      if (tasks.id === id) {
+        return { id, title: updatedTitle, taskDesc: updatedTaskDesc };
       }
       return tasks;
-    })
+    });
     setTasks(updateTask);
-  }
+  };
   return (
     <div className="App">
       <TaksCreate onCreate={createTask} />
       <h1>Görevler</h1>
-      <TaskList tasks={tasks} onDelete={deleteTaskById} onUpdate={editTaskById} />
+      <TaskList
+        tasks={tasks}
+        onDelete={deleteTaskById}
+        onUpdate={editTaskById}
+      />
     </div>
   );
 }
